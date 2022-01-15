afd885663e2033b63ac658a806af1b995f11c323
Florian Koch <flkoch@dev.fkoch.ch>
2022-01-15 23:36:54 +0100

    Added certificates for users and restrictions to trainings. Users can only register if they have a valid certificate for any of the restrictions.

--- training_planner/trainings/admin.py
+++ training_planner/trainings/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Address, Location, TargetGroup, Training
+from .models import Address, Location, Restriction, TargetGroup, Training
 
 admin.site.register(Training)
 admin.site.register(TargetGroup)
 admin.site.register(Location)
 admin.site.register(Address)
+admin.site.register(Restriction)
